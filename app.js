5788edc8a7a1dbfcf75ad36185edf250eb72b56c
Tn6o <tn6o.dev@gmail.com>
2012-12-02 16:34:06 +0100

    Addition of Director and resourceful

--- app.js
+++ app.js
@@ -2,11 +2,9 @@ var flatiron = require('flatiron'),
     path = require('path'),
     app = flatiron.app,
     router = require('./routes.js');
-
 app.config.file({ file: path.join(__dirname, 'config', 'config.json') });
 
 app.use(flatiron.plugins.http);
-
 
 router.match('/', 'index.html');
 router.match('/chat', 'chat.html');

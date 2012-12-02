bc6030a79e2b97c34b34d826d47cdf4dda5506c7
Tn6o <tn6o.dev@gmail.com>
2012-12-02 02:29:37 +0100

    Routing for / to get index.html

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 var flatiron = require('flatiron'),
     path = require('path'),
-    app = flatiron.app;
+    app = flatiron.app,
+    fs = require('fs');
 
 app.config.file({ file: path.join(__dirname, 'config', 'config.json') });
 
 app.use(flatiron.plugins.http);
 
 app.router.get('/', function () {
-  
-
-
+  var self = this;
+  fs.readFile('index.html', function(err, data) {
+    if(err) {
+      Console.log(err);
+      self.res.writeHead(404);
+      self.res.end();
+      return;
+    }
+    self.res.writeHead(200, {'Content-Type': 'text/html'});
+    self.res.end(data);
+  })
 });
 
 app.start(3000);

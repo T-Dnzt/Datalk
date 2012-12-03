dcba073a46cbd3c76fb29491a3ecbaaa12002e2c
Tn6o <tn6o.dev@gmail.com>
2012-12-03 14:07:04 +0100

    Add gitignore and delete node_modules

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 var flatiron = require('flatiron'),
     path = require('path'),
     app = flatiron.app,
-    router = require('./routes.js');
+    routes = require('./routes.js');
+
 app.config.file({ file: path.join(__dirname, 'config', 'config.json') });
 
 app.use(flatiron.plugins.http);
 
-router.match('/', 'index.html');
-router.match('/chat', 'chat.html');
+routes.match('/', 'index.html');
+routes.match('/chat', 'chat.html');
 
 app.start(3000);

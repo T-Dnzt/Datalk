c33a34b899b6bc4cee9f1cf36f3e4a5a7e01be62
Tn6o <tn6o.dev@gmail.com>
2012-12-04 18:22:54 +0100

    Save talk in mongodb after 5 minutes

--- app.js
+++ app.js
@@ -18,11 +18,57 @@ routes.match('/app', 'app.css', 'text/css');
 
 app.start(3000);
 
+var collectionTalks = null;
+database.open(function(err, client) {
+    client.collection('talks', function(err, collection) {
+        collectionTalks = collection;
+    });
+});
+
 var io = require('socket.io').listen(app.server);
 
 var users = [];
 var messages = [];
 var talk = [];
+var timeOut = null;
+
+
+var saveTalk = function() {
+
+    console.log("Save Talk");
+    var firstAuthor = talk[0].author;
+    var messageID = "";
+    var permalink = "";
+
+    for(var i = 0; i < talk.length; i++)
+    {
+       message = talk[i];
+       if(message.content.split(' ').length > 5 && message.author == firstAuthor)
+       {
+            messageID = message.content;
+            break;
+       }
+    }
+
+    if(messageID.length > 0)
+    {
+        console.log(messageID);
+        var formattedMessageID = messageID.replace(/ /g,"-").replace(/\W/g, '-');
+        console.log(formattedMessageID);
+        permalink = formattedMessageID + "_" + firstAuthor;
+    }   
+    else
+    {
+        permalink = firstAuthor;
+    }
+
+    collectionTalks.insert({'first_author' : firstAuthor,
+                           'sentence_id' : messageID,
+                           'messages' : talk,
+                           'permalink' : permalink });
+
+    talk = [];
+};
 
 io.sockets.on('connection', function(socket) {
 
@@ -35,7 +81,15 @@ io.sockets.on('connection', function(socket) {
 
     socket.on('send-message', function(message) {
         messages.push(message);
+        talk.push(message);
         io.sockets.emit("new-message", message);
+        if(timeOut) {
+            clearTimeout(timeOut);
+            timeOut = setTimeout(saveTalk, 300000);
+        }
+        else {
+            timeOut = setTimeout(saveTalk, 300000);
+        }
     });
 
     socket.on('disconnect', function() {
